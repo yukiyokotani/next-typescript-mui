acf830ca401cc61e2a80f4187025bbeb8a4f437a
Yuki Yokotani <yokotani.yuki@gmail.com>
2022-09-08 02:41:19 +0900

    Layoutの修正

--- components/global/Layout.tsx
+++ components/global/Layout.tsx
@@ -22,8 +22,6 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
     padding: '20px',
     marginTop: `${headerHeight}px`,
     ...(!open && {
-      borderBottomLeftRadius: 0,
-      borderBottomRightRadius: 0,
       transition: theme.transitions.create('margin', {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen
@@ -33,15 +31,12 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
         width: `calc(100% - ${drawerWidth}px)`
       },
       [theme.breakpoints.down('md')]: {
-        marginLeft: '20px',
         width: `calc(100% - ${drawerWidth}px)`,
         padding: '16px'
       },
       [theme.breakpoints.down('sm')]: {
-        marginLeft: '10px',
         width: `calc(100% - ${drawerWidth}px)`,
-        padding: '16px',
-        marginRight: '10px'
+        padding: '16px'
       }
     }),
     ...(open && {
@@ -49,13 +44,14 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen
       }),
-      marginLeft: 0,
       width: `calc(100% - ${drawerWidth}px)`,
       [theme.breakpoints.down('md')]: {
-        marginLeft: '20px'
+        width: `calc(100% - ${drawerWidth}px)`,
+        padding: '16px'
       },
       [theme.breakpoints.down('sm')]: {
-        marginLeft: '10px'
+        width: `calc(100% - ${drawerWidth}px)`,
+        padding: '16px'
       }
     })
   })
